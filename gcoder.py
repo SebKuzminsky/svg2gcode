f2b641123a83709f33ffb4904531bf5370371dc9
Sebastian Kuzminsky <seb@highlab.com>
2016-11-03 17:14:07 -0600

    add a drill_hog() function, to pre-rough out a pocket with a drill
    Only works on rectangular pockets for now.

--- gcoder.py
+++ gcoder.py
@@ -1,3 +1,5 @@
+import math
+
 
 def feed(feed_rate_units_per_minute):
     print "F %.4f" % feed_rate_units_per_minute
@@ -51,3 +53,61 @@ def g83(retract, delta, x=None, y=None, z=None):
     print "R%.4f" % retract,
     print "Q%.4f" % delta,
     print
+
+
+def drill_hog(diameter, retract, delta, z_drill, x0, y0, x1, y1, finishing_allowance=None):
+
+    """Drills as many holes as will fit in a rectangular grid, within the
+    rectangle defined by (x0, y0) and (x1, y1).  If finishing_tolerance
+    is specified, all the holes will stay at least that far away from
+    the edges of the specified rectangle."""
+
+    radius = diameter/2.0
+
+    min_x = min(x0, x1)
+    max_x = max(x0, x1)
+
+    min_y = min(y0, y1)
+    max_y = max(y0, y1)
+
+    if finishing_allowance != None:
+        min_x = min_x + finishing_allowance
+        max_x = max_x - finishing_allowance
+        min_y = min_y + finishing_allowance
+        max_y = max_y - finishing_allowance
+
+    x_range = max_x - min_x
+    y_range = max_y - min_y
+
+    num_in_x = int(math.floor(x_range / diameter))
+    num_in_y = int(math.floor(y_range / diameter))
+
+    min_x = min_x + radius
+    max_x = max_x - radius
+
+    min_y = min_y + radius
+    max_y = max_y - radius
+
+    x_range = x_range - diameter
+    y_range = y_range - diameter
+
+    for x_index in range(0, num_in_x):
+        if num_in_x > 1:
+            x = min_x + ((x_index / float(num_in_x - 1)) * x_range)
+        else:
+            x = min_x + (x_range / 2.0)
+
+        for y_index in range(0, num_in_y):
+            if num_in_y > 1:
+                y = min_y + ((y_index / float(num_in_y - 1)) * y_range)
+            else:
+                y = min_y + (y_range / 2.0)
+
+            print "G83 ",
+            print "X%.4f" % x,
+            print "Y%.4f" % y,
+            print "Z%.4f" % z_drill,
+            print "R%.4f" % retract,
+            print "Q%.4f" % delta,
+            print
+
--- test.py
+++ test.py
@@ -16,3 +16,6 @@ path.append({'z':1})
 gcoder.g1(path=path)
 
 gcoder.g83(x=1, y=2, z=-3, retract=0.1, delta=0.2)
+
+gcoder.drill_hog(diameter=0.250, retract=0.1, delta=0.2, z_drill=-1, x0=0, y0=0, x1=-1, y1=2)
+gcoder.drill_hog(diameter=0.250, retract=0.1, delta=0.2, z_drill=-1, x0=0, y0=0, x1=-1, y1=0.506, finishing_allowance=0.001)
