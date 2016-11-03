b656a36a0a5ef8c4516805eaddcb470c72e5e5f0
Sebastian Kuzminsky <seb@highlab.com>
2016-11-02 21:27:04 -0600

    beginnings of "gcoder" project

---
+++ gcoder.py
@@ -0,0 +1,43 @@
+
+def feed(feed_rate_units_per_minute):
+    print "F %.4f" % feed_rate_units_per_minute
+
+def speed(spindle_rpm):
+    print "S %.4f" % spindle_rpm
+
+
+def g0(path=None, x=None, y=None, z=None, a=None, b=None, c=None, u=None, v=None, w=None):
+    if path is not None:
+        for waypoint in path:
+            g0(**waypoint)
+    else:
+        print "G0",
+        if x is not None: print "X%.4f" % x,
+        if y is not None: print "Y%.4f" % y,
+        if z is not None: print "Z%.4f" % z,
+        if a is not None: print "A%.4f" % a,
+        if b is not None: print "B%.4f" % b,
+        if c is not None: print "C%.4f" % c,
+        if u is not None: print "U%.4f" % u,
+        if v is not None: print "V%.4f" % v,
+        if w is not None: print "W%.4f" % w,
+        print
+
+
+def g1(path=None, x=None, y=None, z=None, a=None, b=None, c=None, u=None, v=None, w=None):
+    if path is not None:
+        for waypoint in path:
+            g1(**waypoint)
+    else:
+        print "G1",
+        if x is not None: print "X%.4f" % x,
+        if y is not None: print "Y%.4f" % y,
+        if z is not None: print "Z%.4f" % z,
+        if a is not None: print "A%.4f" % a,
+        if b is not None: print "B%.4f" % b,
+        if c is not None: print "C%.4f" % c,
+        if u is not None: print "U%.4f" % u,
+        if v is not None: print "V%.4f" % v,
+        if w is not None: print "W%.4f" % w,
+        print
+
---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import gcoder
+
+gcoder.feed(1.23)
+gcoder.g1(x=1, a=123)
+
+path = []
+path.append({'x':0, 'y':0})
+path.append({'z':-1})
+path.append({'x':2, 'y':0})
+path.append({'x':2, 'y':2})
+path.append({'x':0, 'y':2})
+path.append({'x':0, 'y':0})
+path.append({'z':1})
+gcoder.g1(path=path)
+
