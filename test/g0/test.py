126d531753c78dca6038ac2050b1ef703f59b416
Sebastian Kuzminsky <seb@highlab.com>
2017-12-09 14:37:04 -0700

    make runtests able to handle multiple tests

---
+++ test/g0/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+import gcoder
+
+gcoder.g0(x=1)
+gcoder.g0(y=2)
+gcoder.g0(z=3)
+gcoder.g0(a=4)
+gcoder.g0(b=5)
+gcoder.g0(c=6)
+gcoder.g0(u=7)
+gcoder.g0(v=8)
+gcoder.g0(w=9)
+
+gcoder.g0(x=0.1, y=0.2)
+gcoder.g0(x=0.1, z=0.3)
+gcoder.g0(y=0.2, z=0.3)
+
