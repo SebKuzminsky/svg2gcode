b71ae8dba1efe94fe688a2473b0a62cfb7da3b87
Sebastian Kuzminsky <seb@highlab.com>
2017-12-28 13:14:29 -0700

    test: better infrastructure for offset_path()

--- test/offset-path/rounded-square-1/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-../../../svg2gcode --offset 1 ../rounded-square-equal-radii.svg >| result.ngc
-diff -u expected.ngc result.ngc || exit 1
-
-rs274 -t /dev/null -v /dev/null -g result.ngc >| result.canon
-diff -u expected.canon result.canon || exit 1
