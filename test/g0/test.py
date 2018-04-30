0c6a2979cf68777186bbb0ee7602852784c7b082
Sebastian Kuzminsky <seb@highlab.com>
2018-04-29 19:14:23 -0600

    tweak the g0 test to pass with the new sai verification

--- test/g0/test.py
+++ test/g0/test.py
@@ -8,11 +8,9 @@ gcoder.g0(z=3)
 gcoder.g0(a=4)
 gcoder.g0(b=5)
 gcoder.g0(c=6)
-gcoder.g0(u=7)
-gcoder.g0(v=8)
-gcoder.g0(w=9)
 
 gcoder.g0(x=0.1, y=0.2)
 gcoder.g0(x=0.1, z=0.3)
 gcoder.g0(y=0.2, z=0.3)
 
+gcoder.m2()
