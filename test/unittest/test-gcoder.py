fc6ee1bd085611515c3dc8289ccda141e20aaf68
Sebastian Kuzminsky <seb@highlab.com>
2018-12-04 13:49:34 -0700

    add a unit tests of gcoder.split_path_at_intersections()
    Also minor fixes to runtests, to support the new unit test.

---
+++ test/unittest/test-gcoder.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'svgpathtools'))
+import svgpathtools
+
+import gcoder
+
+
+class test_split_path_at_intersections(unittest.TestCase):
+
+    def test_no_intersection(self):
+        p = svgpathtools.path.Path()
+        p.append(svgpathtools.path.Line((0+0j), (10+0j)))
+        p.append(svgpathtools.path.Line((10+0j), (10+10j)))
+        p.append(svgpathtools.path.Line((10+10j), (0+10j)))
+        p.append(svgpathtools.path.Line((0+10j), (0+0j)))
+
+        out = gcoder.split_path_at_intersections(p, debug=True)
+
+        self.assertEqual(len(out), 1)
+        self.assertEqual(svgpathtools.path.Path(*out[0]), p)
+
+
+if __name__ == '__main__':
+    unittest.main()
