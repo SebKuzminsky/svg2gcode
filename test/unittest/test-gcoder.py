48a4b975f49ab132f0ca226a294bb123935ee459
Eric Cavard <eric.cavard@protonmail.ch>
2021-09-18 18:50:10 +0200

    run tests in parallel and test GRBL with a physical arduino board only

--- test/unittest/test-gcoder.py
+++ test/unittest/test-gcoder.py
@@ -4,7 +4,6 @@ import unittest
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'svgpathtools'))
 import svgpathtools
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
